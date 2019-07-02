4dbe0362068953e5a9464e8fa9392deeaa524206
wjmarcolin@hotmail.com <wjmarcolin@hotmail.com>
2019-07-02 11:47:37 -0300

    criacao de 2 horarios diferentes

--- application/models/Classroom_model.php
+++ application/models/Classroom_model.php
@@ -5,7 +5,8 @@ class Classroom_model extends CI_Model
     {
         parent::__construct();
     }
-    public function criarConta($data){
+    public function criarConta($data)
+    {
         $this->db->insert('user', $data);
         return $this->db->insert_id();
     }
@@ -15,12 +16,14 @@ class Classroom_model extends CI_Model
         classroom.id as cid,
         professores.nome as tname,
         disciplinas.nome as sname,
-        classroom.number as number,
         campus.name as campus,
         addresses.building as building,
         addresses.address as address,
         period.name as period,
         GROUP_CONCAT(distinct week_day.name order by week_day.id ASC SEPARATOR "," ) as wdays,
+        GROUP_CONCAT(distinct classroom_week_day.room order by week_day.id ASC SEPARATOR ",") as rooms,
+        GROUP_CONCAT(distinct classroom_week_day.start_time order by week_day.id ASC SEPARATOR ",") as start_times,
+        GROUP_CONCAT(distinct classroom_week_day.end_time order by week_day.id ASC SEPARATOR ",") as end_times,
         classroom_week_day.start_time,
         classroom_week_day.end_time,
         addresses.iframe as maps_info
@@ -34,11 +37,12 @@ class Classroom_model extends CI_Model
         $this->db->join('week_day', 'week_day.id = classroom_week_day.week_day_id');
         $this->db->join('addresses', 'addresses.id = classroom.address_id', 'left');
         $this->db->join('campus', 'campus.id = addresses.campus_id', 'left');
-        $this->db->group_by(array("professores.id", "disciplinas.id", "classroom_week_day.start_time"));
+        //$this->db->group_by(array("professores.id", "disciplinas.id", "classroom_week_day.start_time"));
+        $this->db->group_by(array("classroom.id"));
         if (!$param) {
             $query = $this->db->get();
             return $query;
-        } else if ((int)$param) {
+        } else if ((int) $param) {
             $this->db->where('student_classroom.user_id', $param);
         } else {
             $this->db->like('disciplinas.nome', $param, 'both');
@@ -56,12 +60,14 @@ class Classroom_model extends CI_Model
         classroom.id as cid,
         professores.nome as tname,
         disciplinas.nome as sname,
-        classroom.number as number,
         campus.name as campus,
         addresses.building as building,
         addresses.address as address,
         period.name as period,
         GROUP_CONCAT(distinct week_day.name order by week_day.id ASC SEPARATOR "," ) as wdays,
+        GROUP_CONCAT(distinct classroom_week_day.room order by week_day.id ASC SEPARATOR ",") as rooms,
+        GROUP_CONCAT(distinct classroom_week_day.start_time order by week_day.id ASC SEPARATOR ",") as start_times,
+        GROUP_CONCAT(distinct classroom_week_day.end_time order by week_day.id ASC SEPARATOR ",") as end_times,
         classroom_week_day.start_time,
         classroom_week_day.end_time,
         addresses.iframe as maps_info
@@ -103,13 +109,15 @@ class Classroom_model extends CI_Model
                 'professor' => $row->tname,
                 'horario_ini' => $row->start_time,
                 'horario_fim' => $row->end_time,
+                'horarios_ini' => $row->start_times,
+                'horarios_fim' => $row->end_times,
                 'dia' => $row->wdays,
                 'campus' => $row->campus,
                 'predio' => $row->building,
-                'sala' => $row->number,
+                'sala' => $row->rooms,
                 'endereco' => $row->address,
                 'periodo' => $row->period,
-                'maps' =>$row->maps_info
+                'maps' => $row->maps_info,
             );
             $i++;
         }
@@ -203,43 +211,55 @@ class Classroom_model extends CI_Model
         $options = $this->db->get('period')->result();
         return $this->getOptionsAsDropdown($options, 'o Período');
     }
-    public function getAddressesAsDropdown(){
+    public function getAddressesAsDropdown()
+    {
         $this->db->select('id, building as name');
         $options = $this->db->get('addresses')->result();
         return $this->getOptionsAsDropdown($options, 'o Prédio');
     }
-    public function getCampusAsDropdown(){
+    public function getCampusAsDropdown()
+    {
         $this->db->select('id, name');
         $options = $this->db->get('campus')->result();
         return $this->getOptionsAsDropdown($options, 'o Campus');
     }
-    
+
     //CRUD TURMA
     public function updateClassroom($data)
     {
         $values = array(
             'teacher_id' => $data["teacher_id"],
             'subject_id' => $data["subject_id"],
-            'campus' => $data["campus"],
-            'building' => $data["building"],
-            'number' => $data["number"],
-            'address' => $data["address"],
             'period_id' => $data["period_id"],
-            'maps_info' => $data["maps_info"]
+            'address_id' => $data["building"],
         );
         $this->db->where('id', $data["id"]);
         $this->db->update('classroom', $values);
 
         $this->db->delete('classroom_week_day', array('classroom_id' => $data["id"]));
-        $count = count($data['week_day']);
+        $count = count($data['1week_day']);
         for ($i = 0; $i < $count; $i++) {
             $time = array(
-                'classroom_id' => $data['id'],
-                'week_day_id' => $data['week_day'][$i],
+                'classroom_id' => $data["id"],
+                'week_day_id' => $data['1week_day'][$i],
                 'start_time' => $data['start_time'],
-                'end_time' => $data['end_time']
+                'end_time' => $data['end_time'],
+                'room' => $data['room']
             );
             $this->db->insert('classroom_week_day', $time);
+        }
+        if ($data['schedule-select'] == 2) {
+            $count = count($data['2week_day']);
+            for ($i = 0; $i < $count; $i++) {
+                $time = array(
+                    'classroom_id' => $data["id"],
+                    'week_day_id' => $data['2week_day'][$i],
+                    'start_time' => $data['2start_time'],
+                    'end_time' => $data['2end_time'],
+                    'room' => $data['2room']
+                );
+                $this->db->insert('classroom_week_day', $time);
+            }
         }
 
     }
@@ -249,25 +269,37 @@ class Classroom_model extends CI_Model
             'teacher_id' => $data['teacher_id'],
             'subject_id' => $data['subject_id'],
             'period_id' => $data['period_id'],
-            'campus' => $data['campus'],
-            'building' => $data['building'],
-            'address' => $data['address'],
-            'number' => $data['number']
+            'address_id' => $data['building'],
         );
         $this->db->insert('classroom', $values);
         $last_id = $this->db->insert_id();
-        $count = count($data['week_day']);
+        $count = count($data['1week_day']);
         for ($i = 0; $i < $count; $i++) {
             $time = array(
                 'classroom_id' => $last_id,
-                'week_day_id' => $data['week_day'][$i],
+                'week_day_id' => $data['1week_day'][$i],
                 'start_time' => $data['start_time'],
-                'end_time' => $data['end_time']
+                'end_time' => $data['end_time'],
+                'room' => $data['room']
             );
             $this->db->insert('classroom_week_day', $time);
         }
+        if ($data['schedule-select'] == 2){
+            $count = count($data['2week_day']);
+            for ($i = 0; $i < $count; $i++) {
+                $time = array(
+                    'classroom_id' => $last_id,
+                    'week_day_id' => $data['2week_day'][$i],
+                    'start_time' => $data['2start_time'],
+                    'end_time' => $data['2end_time'],
+                    'room' => $data['2room']
+                );
+                $this->db->insert('classroom_week_day', $time);
+            }
+        }
     }
-    public function deleteClassroom($id){
+    public function deleteClassroom($id)
+    {
         $this->db->delete('classroom_week_day', array('classroom_id' => $id));
         $this->db->delete('student_classroom', array('classroom_id' => $id));
         $this->db->delete('classroom', array('id' => $id));
@@ -276,10 +308,9 @@ class Classroom_model extends CI_Model
     //CRUD SUBJECT
     public function getSubjectByName($data)
     {
-        $this->db->select('*, nome as name, cod_disc as code');
+        $this->db->select('*, disciplinas.nome as name, cod_disc as code');
         $this->db->from('disciplinas');
-        $this->db->like('nome', $data, 'both');
-        $this->db->order_by('nome', 'asc');
+        $this->db->order_by('disciplinas.nome', 'asc');
         return $this->db->get()->result();
     }
     public function updateSubject($data)
@@ -303,11 +334,12 @@ class Classroom_model extends CI_Model
         );
         $this->db->insert('disciplinas', $values);
     }
-    public function getClassroomName($id){
+    public function getClassroomName($id)
+    {
         $this->db->select('nome as name');
         $this->db->from('disciplinas');
         $this->db->join('classroom', 'classroom.subject_id = disciplinas.id');
-        $this->db->where('classroom.id',$id );
+        $this->db->where('classroom.id', $id);
         return $this->db->get()->row();
     }
 
@@ -343,7 +375,7 @@ class Classroom_model extends CI_Model
         }
         $this->db->insert('professores', $values);
     }
-    
+
     //CRUD PERIOD
     public function getPeriodByName($data)
     {
@@ -372,7 +404,8 @@ class Classroom_model extends CI_Model
         $this->db->insert('period', $values);
     }
 
-    public function insertStudentclassroom($userId, $classroomId){
+    public function insertStudentclassroom($userId, $classroomId)
+    {
         $data = array(
             'user_id' => $userId,
             'classroom_id' => $classroomId,
@@ -380,7 +413,8 @@ class Classroom_model extends CI_Model
         $this->db->insert('student_classroom', $data);
 
     }
-    public function removeStudentclassroom($userId, $classroomId){
+    public function removeStudentclassroom($userId, $classroomId)
+    {
         $data = array(
             'user_id' => $userId,
             'classroom_id' => $classroomId,
@@ -388,7 +422,8 @@ class Classroom_model extends CI_Model
         $this->db->delete('student_classroom', $data);
 
     }
-    public function getEmailByClassroom($cid){
+    public function getEmailByClassroom($cid)
+    {
         $this->db->select('alunos.email as email, alunos.nome as name', false);
         $this->db->from('student_classroom');
         $this->db->join('alunos', 'alunos.id = student_classroom.user_id');
@@ -398,53 +433,60 @@ class Classroom_model extends CI_Model
 
     //CRUD addresses
 
-    public function getAddresses($like = null, $where = null){
+    public function getAddresses($like = null, $where = null)
+    {
         $this->db->select('a.id as id, campus_id, c.name as campus, building, iframe, address');
         $this->db->from('addresses a');
         $this->db->join('campus c', 'a.campus_id = c.id');
-        if (!is_null($like) && is_null($where)){
+        if (!is_null($like) && is_null($where)) {
             $this->db->like('c.name', $like, 'both');
             $this->db->or_like('a.building', $like, 'both');
         }
-        if (is_null($like) && !is_null($where)){
+        if (is_null($like) && !is_null($where)) {
             $this->db->where('a.campus_id', $where);
         }
         return $this->db->get();
     }
 
-    public function searchAddresses($query){
+    public function searchAddresses($query)
+    {
         return $this->getAddresses($query, null);
     }
 
-    public function getAddressesByCampusId($id){
+    public function getAddressesByCampusId($id)
+    {
         return $this->getAddresses(null, $id);
     }
 
-    public function getCampus(){
+    public function getCampus()
+    {
         return $this->db->get('campus');
     }
 
-    public function updateAddresses($data){
+    public function updateAddresses($data)
+    {
         $values = array(
             'campus_id' => $data["campus"],
             'building' => $data["building"],
             'iframe' => $data["iframe"],
-            'address' => $data["address"]
+            'address' => $data["address"],
         );
         $this->db->where('id', $data["id"]);
         $this->db->update('addresses', $values);
     }
 
-    public function deleteAddresses($data){
+    public function deleteAddresses($data)
+    {
         $this->db->delete('addresses', array('id' => $data["id"]));
     }
 
-    public function createAddresses($data){
+    public function createAddresses($data)
+    {
         $values = array(
             'campus_id' => $data["campus"],
             'building' => $data["building"],
             'iframe' => $data["iframe"],
-            'address' => $data["address"]
+            'address' => $data["address"],
         );
         $this->db->insert('addresses', $values);
     }
